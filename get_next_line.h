b3155e93f89ebcf1284d0b7b3f774f6d58a1b244
Felipe Azevedo Soares Suassuna <suassuna@Felipes-MacBook-Pro.local>
2025-04-24 04:43:45 +0100

    final

--- get_next_line.h
+++ get_next_line.h
@@ -1,14 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: feazeved <feazeved@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/21 17:07:35 by feazeved          #+#    #+#             */
-/*   Updated: 2025/04/24 03:16:27 by feazeved         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
@@ -16,17 +6,23 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
 # endif
-
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
 
-char    *ft_strjoin_and_free(char *old, char *buffer);
-int ft_strlen(char *str);
-int ft_newline(char *buffer);
-char    *ft_read_until_line(char *str, int fd);
-char    *ft_get_line(char *str);
-char    *ft_clean_line(char *str);
-char    *get_next_line(int fd);
+typedef struct s_list
+{
+  int fd;
+  char  *buffer;
+  struct s_list *next; 
+} t_list;
+
+t_list *ft_get_fd(int fd, t_list **head);
+void ft_free_node(t_list **head, int fd);
+int ft_isnewline(char *buffer);
+char *ft_clean(char *buffer);
+char *ft_line(char *buffer);
+char *ft_strjoin_and_free(char *old, const char *temp);
+char  *get_next_line(int fd);
 
 #endif
