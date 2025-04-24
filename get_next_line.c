61faf698eda477422f9061f24de52b43e4dc0d2f
Felipe Azevedo Soares Suassuna <feazeved@c1r4s5.42porto.com>
2025-04-24 04:18:28 +0100

    fds

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,13 @@
 /*   By: feazeved <feazeved@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:06:49 by feazeved          #+#    #+#             */
-/*   Updated: 2025/04/21 22:36:27 by feazeved         ###   ########.fr       */
+/*   Updated: 2025/04/24 04:06:57 by feazeved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*ft_line(char *buffer)
-{
-	int		i;
-	int		j;
-	char	*line;
-
-	if (!buffer)
-		return (NULL);
-	i = 0;
-	while (buffer[i] && buffer[i] != '\n')
-		i++;
-	if (buffer[i] == '\n')
-		i++;
-	line = malloc(sizeof(char) * (i + 1));
-	if (!line)
-		return (NULL);
-	j = 0;
-	while (buffer[j] && buffer[j] != '\n')
-	{
-		line[j] = buffer[j];
-		j++;
-	}
-	if (buffer[j] == '\n')
-		line[j++] = '\n';
-	line[j] = '\0';
-	return (line);
-}
-
-char	*ft_strjoin_and_free(char *old, char *temp)
+char	*ft_strjoin_and_free(char *old, char *buffer)
 {
 	int		i;
 	int		j;
@@ -53,88 +25,176 @@ char	*ft_strjoin_and_free(char *old, char *temp)
 			return (NULL);
 		old[0] = '\0';
 	}
-	i = 0;
-	while (old[i])
-		i++;
-	j = 0;
-	while (temp[j])
-		j++;
-	new = malloc(sizeof(char) * (i + j + 1));
+	new = malloc(sizeof(*new) * (ft_strlen(old) + ft_strlen(buffer) + 1));
 	if (!new)
 		return (free(old), NULL);
-	ft_strcpycat(&new, old, temp);
+	i = -1;
+	while (old[++i])
+		new[i] = old[i];
+	j = -1;
+	while (buffer[++j])
+		new[i + j] = buffer[j];
+	new[i + j] = '\0';
 	return (free(old), new);
 }
 
-void	ft_strcpycat(char **new, char *old, char *temp)
+int	ft_strlen(char *str)
 {
 	int	i;
-	int	j;
 
-	i = -1;
-	j = -1;
-	while (old[++i])
-		(*new)[i] = old[i];
-	while (temp[++j])
-		(*new)[i + j] = temp[j];
-	(*new)[i + j] = '\0';
+	i = 0;
+	while (str && str[i])
+		i++;
+	return (i);
 }
 
-int	ft_initial_conditions(int fd, t_list **head, t_list **node)
+int	ft_newline(char	*buffer)
 {
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	int	i;
+
+	i = 0;
+	if (!buffer)
 		return (0);
-	*node = ft_get_fd(fd, head);
-	if (!(*node))
-		return (0);
-	return (1);
+	while (buffer[i])
+	{
+		if (buffer[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_read_until_line(char *str, int fd)
+{
+	char	buffer[BUFFER_SIZE + 1];
+	int		num_read;
+
+	while (!ft_newline(str))
+	{
+		num_read = read(fd, buffer, BUFFER_SIZE);
+		if (num_read <= 0)
+		{
+			if (num_read == 0 && str && *str)
+				return (str);
+			free(str);
+			return (NULL);
+		}
+		buffer[num_read] = '\0';
+		str = ft_strjoin_and_free(str, buffer);
+		if (!str)
+			return (NULL);
+	}
+	return (str);
+}
+
+char	*ft_get_line(char *str)
+{
+	int		i;
+	char	*line;
+
+	if (!str || !*str)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		i++;
+	line = malloc(sizeof(*line) * (i + 1));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (str[i] && str[i] != '\n')
+	{
+		line[i] = str[i];
+		i++;
+	}
+	if (str[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+char	*ft_clean_line(char	*str)
+{
+	int		j;
+	int		i;
+	char	*clean_line;
+
+	i = 0;
+	while (str[i] && str[i] != '\n')
+		i++;
+	if (!str[i])
+		return (free(str), NULL);
+	i++;
+	clean_line = malloc(sizeof(*clean_line) * (ft_strlen(str + i) + 1));
+	if (!clean_line)
+		return (free(str), NULL);
+	j = 0;
+	while (str[i])
+		clean_line[j++] = str[i++];
+	clean_line[j] = '\0';
+	return (free(str), clean_line);
 }
 
 char	*get_next_line(int fd)
 {
-	static t_list	*head = NULL;
-	t_list			*node;
-	char			temp[BUFFER_SIZE + 1];
-	int				num_read;
+	static	char	*str[1024];
 	char			*line;
 
-	if (!ft_initial_conditions(fd, &head, &node))
-		return (NULL);
-	while (!ft_isnewline(node->buffer))
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 	{
-		num_read = read(fd, temp, BUFFER_SIZE);
-		if (num_read <= 0)
-			break ;
-		temp[num_read] = '\0';
-		node->buffer = ft_strjoin_and_free(node->buffer, temp);
-		if (!node->buffer)
-			return (ft_free_node(&head, fd), NULL);
+		if (fd >= 0 && fd < 1024 && str[fd])
+		{
+			free(str[fd]);
+			return (NULL);
+		}
 	}
-	if (!node->buffer || node->buffer[0] == '\0')
-		return (ft_free_node(&head, fd), NULL);
-	line = ft_line(node->buffer);
-	node->buffer = ft_clean(node->buffer);
-	if (!node->buffer)
-		ft_free_node(&head, fd);
+	str[fd] = ft_read_until_line(str[fd], fd);
+	if (!str[fd])
+		return (NULL);
+	line = ft_get_line(str[fd]);
+	if (!line)
+	{
+		free(str[fd]);
+		str[fd] = NULL;
+		return (NULL);
+	}
+	str[fd] = ft_clean_line(str[fd]);
 	return (line);
 }
-/*
-#include <stdio.h> 
+
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+
 int main(int argc, char *argv[])
 {
 	int	fd;
 	int	lines;
 	int	i = 0;
 	char	*retorno;
+//	long	open_max = sysconf(_SC_OPEN_MAX);
 
+//	printf("OPEN_MAX = %ld\n", open_max);
+
+	(void)argc;
 	lines = atoi(argv[2]);
 	fd = open(argv[1], O_RDONLY);
-	while (i++ != lines)
+	while (i++ < lines)
 	{
 		retorno = get_next_line(fd);
+		if (!retorno)
+			break;
 		printf("%s", retorno);
 		free(retorno);
 	}
+/*	while (1)
+	{
+		retorno = get_next_line(fd);
+		if (!retorno)
+			break;
+		free(retorno);
+	}*/
 	close(fd);
 	return (0);
-}*/
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: feazeved <feazeved@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 17:07:35 by feazeved          #+#    #+#             */
-/*   Updated: 2025/04/21 22:38:37 by feazeved         ###   ########.fr       */
+/*   Updated: 2025/04/24 03:16:27 by feazeved         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,12 @@
 # include <fcntl.h>
 # include <stdlib.h>
 
-typedef struct s_list
-{
-	int				fd;
-	char			*buffer;
-	struct s_list	*next;
-}	t_list;
-
-t_list	*ft_get_fd(int fd, t_list **head);
-void	ft_free_node(t_list **head, int fd);
-int		ft_isnewline(char *buffer);
-char	*ft_clean(char *buffer);
-char	*ft_line(char *buffer);
-char	*ft_strjoin_and_free(char *old, char *temp);
-char	*get_next_line(int fd);
-int		ft_initial_conditions(int fd, t_list **head, t_list **node);
-void	ft_strcpycat(char **new, char *temp, char *old);
+char    *ft_strjoin_and_free(char *old, char *buffer);
+int ft_strlen(char *str);
+int ft_newline(char *buffer);
+char    *ft_read_until_line(char *str, int fd);
+char    *ft_get_line(char *str);
+char    *ft_clean_line(char *str);
+char    *get_next_line(int fd);
 
 #endif
